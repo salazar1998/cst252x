9820d1c38a684a1778244ba731a87f5706452632
Carlos Salazar <carlossalazar@Carloss-MacBook-Pro-2.local>
2021-09-13 16:29:17 -0700

    lab 5 done

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Carlos Salazar <carsalazar@csumb.edu>
+ * Created: September 13, 2021
+ * License: Public Domain
+ */
+ //Define variables to hold your make , model, color, an year
+var make = "Honda";
+var model = "civic";
+var color = "grey";
+var year = 2008;
+
+// calculate
+var age = 2021 - year;
+
+var ownit= ture;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year<br>");
