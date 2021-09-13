54e605a191e2dc2be77b216ebdc95cb38058f194
Carlos Salazar <carlossalazar@Carloss-MacBook-Pro-2.local>
2021-09-13 00:54:45 -0700

    lab 4 done

---
+++ lab4/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+
+//Pseudocode for how to use a microwave
+        //Print Read the user’s manual to know more about your microwave
+          //Make user click manual and print through away user’s manual because you know you will never use it again
+                    //Dislay plug and make user plug in the microwave
+                    // the food in a microwave-safe utensil only
+      //Ensure that the utensil is microwave-safe otherwise it will explode your microwave
+                    //make user Put food inside the microwave
+                    //make user Close the microwave
+            // Set a right timer for the exact food you are reheating
+      // Press start and let the food heat up until the timer goes off
+                    //Extract food from the microwave.
+                    // Enjoy
